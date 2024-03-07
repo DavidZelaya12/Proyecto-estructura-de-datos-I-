e061af4f6027b72834e7bd80ad918a0e276b410c
Josue <123841868+josuercz@users.noreply.github.com>
2024-03-07 17:41:09 -0600

    mejoras al sistema de login

--- Proyecto/cframe.cpp
+++ Proyecto/cframe.cpp
@@ -8,17 +8,58 @@ cframe::cframe(QWidget *parent)
     ui->setupUi(this);
     ui->tabWidget->setTabEnabled(0, true);
     ui->tabWidget->setTabEnabled(1, false);
+    ui->tabWidget->setTabEnabled(2, false);
+    ui->tabWidget->setTabEnabled(3, false);
+    ui->tabWidget->setTabEnabled(4, false);
 
-    users.push_back(user("david", "123"));
-    users.push_back(user("miguel", "123"));
-    users.push_back(user("daniel", "123"));
-    users.push_back(user("abraham", "123"));
-    users.push_back(user("josue", "123"));
+    docentes.push_back(user("erick", "123"));
+    docentes.push_back(user("victor", "123"));
+    docentes.push_back(user("carlos", "123"));
+
+    coordinadores.push_back(user("carlos", "1234"));
+
+    IEDDs.push_back(user("miguel", "123"));
+    IEDDs.push_back(user("daniel", "123"));
+
+    consultores.push_back(user("abraham", "123"));
+    consultores.push_back(user("david", "123"));
+
 }
 
 cframe::~cframe()
 {
     delete ui;
+}
+
+void cframe::buscarDocentes(QString username, QString password)
+{
+    for (const user& usuario : docentes) {
+        if (usuario.getUsername() == username) {
+            userFound = true;
+            if (usuario.getPassword() == password) {
+                ui->tabWidget->setTabEnabled(0, false);
+                ui->tabWidget->setTabEnabled(1, true);
+                ui->tabWidget->setTabEnabled(2, false);
+                ui->tabWidget->setTabEnabled(3, false);
+                ui->tabWidget->setTabEnabled(4, false);
+                return;
+            } else {
+                QMessageBox::warning(this, "Error", "Contraseña incorrecta");
+                return;
+            }
+        }
+    }
+}
+
+void cframe::logout()
+{
+    ui->LE_Username->clear();
+    ui->LE_Password->clear();
+    ui->tabWidget->setTabEnabled(0, true);
+    ui->tabWidget->setTabEnabled(1, false);
+    ui->tabWidget->setTabEnabled(2, false);
+    ui->tabWidget->setTabEnabled(3, false);
+    ui->tabWidget->setTabEnabled(4, false);
 }
 
 void cframe::on_Btn_Login_clicked()
@@ -31,17 +72,59 @@ void cframe::on_Btn_Login_clicked()
         return;
     }
 
-    bool userFound = false;
-    for (const user& usuario : users) {
-        if (usuario.getUsername() == username) {
-            userFound = true;
-            if (usuario.getPassword() == password) {
-                ui->tabWidget->setTabEnabled(0, false);
-                ui->tabWidget->setTabEnabled(1, true);
-                return;
-            } else {
-                QMessageBox::warning(this, "Error", "Contraseña incorrecta");
-                return;
+    userFound = false;
+
+    if (ui->comboBox_tipo_usuario->currentIndex() == 0) {
+        buscarDocentes(username, password);
+    } else if (ui->comboBox_tipo_usuario->currentIndex() == 1){
+        for (const user& usuario : coordinadores) {
+            if (usuario.getUsername() == username) {
+                userFound = true;
+                if (usuario.getPassword() == password) {
+                    ui->tabWidget->setTabEnabled(0, false);
+                    ui->tabWidget->setTabEnabled(1, false);
+                    ui->tabWidget->setTabEnabled(2, true);
+                    ui->tabWidget->setTabEnabled(3, false);
+                    ui->tabWidget->setTabEnabled(4, false);
+                    return;
+                } else {
+                    QMessageBox::warning(this, "Error", "Contraseña incorrecta");
+                    return;
+                }
+            }
+        }
+    } else if (ui->comboBox_tipo_usuario->currentIndex() == 2){
+        for (const user& usuario : IEDDs) {
+            if (usuario.getUsername() == username) {
+                userFound = true;
+                if (usuario.getPassword() == password) {
+                    ui->tabWidget->setTabEnabled(0, false);
+                    ui->tabWidget->setTabEnabled(1, false);
+                    ui->tabWidget->setTabEnabled(2, false);
+                    ui->tabWidget->setTabEnabled(3, true);
+                    ui->tabWidget->setTabEnabled(4, false);
+                    return;
+                } else {
+                    QMessageBox::warning(this, "Error", "Contraseña incorrecta");
+                    return;
+                }
+            }
+        }
+    } else if (ui->comboBox_tipo_usuario->currentIndex() == 3){
+        for (const user& usuario : consultores) {
+            if (usuario.getUsername() == username) {
+                userFound = true;
+                if (usuario.getPassword() == password) {
+                    ui->tabWidget->setTabEnabled(0, false);
+                    ui->tabWidget->setTabEnabled(1, false);
+                    ui->tabWidget->setTabEnabled(2, false);
+                    ui->tabWidget->setTabEnabled(3, false);
+                    ui->tabWidget->setTabEnabled(4, true);
+                    return;
+                } else {
+                    QMessageBox::warning(this, "Error", "Contraseña incorrecta");
+                    return;
+                }
             }
         }
     }
@@ -51,12 +134,25 @@ void cframe::on_Btn_Login_clicked()
     }
 }
 
-
 void cframe::on_Btn_Logout_clicked()
 {
-    ui->LE_Username->clear();
-    ui->LE_Password->clear();
-    ui->tabWidget->setTabEnabled(0, true);
-    ui->tabWidget->setTabEnabled(1, false);
+    logout();
 }
 
+
+void cframe::on_Btn_Logout_2_clicked()
+{
+    logout();
+}
+
+
+void cframe::on_Btn_Logout_3_clicked()
+{
+    logout();
+}
+
+
+void cframe::on_Btn_Logout_4_clicked()
+{
+    logout();
+}
--- Proyecto/cframe.h
+++ Proyecto/cframe.h
@@ -16,14 +16,27 @@ public:
     cframe(QWidget *parent = nullptr);
     ~cframe();
 
+    void buscarDocentes(QString username, QString password);
+    void logout();
+
 private slots:
     void on_Btn_Login_clicked();
 
     void on_Btn_Logout_clicked();
 
+    void on_Btn_Logout_2_clicked();
+
+    void on_Btn_Logout_3_clicked();
+
+    void on_Btn_Logout_4_clicked();
 
 private:
     Ui::cframe *ui;
-    std::vector<user> users;
+    std::vector<user> docentes;
+    std::vector<user> coordinadores;
+    std::vector<user> IEDDs;
+    std::vector<user> consultores;
+
+    bool userFound;
 };
 #endif // CFRAME_H
